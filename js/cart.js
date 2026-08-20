f373dd201668d09e59fed2aab8e9bcb6301e3381
zcoffren <zcoffren@gmail.com>
2026-08-19 23:02:24 -0400

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -83,6 +83,8 @@ if (item.size) details.push(item.size);
 if (item.placement) details.push(item.placement);
 if (item.option) details.push(item.option);
 if (item.dimensions) details.push(item.dimensions);
+if (item.playerName) details.push('Name: ' + item.playerName);
+if (item.playerNumber) details.push('Number: ' + item.playerNumber);
       
       return `
         <article class="cart-item">
